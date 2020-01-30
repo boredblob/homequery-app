6fc50bfb7cc4326f8fd34e195b3b0d897d325ece
Omer <omerkaraman@hotmail.co.uk>
2020-01-30 22:51:13 +0000

    Error-proofed a little

--- scripts/crypto.mjs
+++ scripts/crypto.mjs
@@ -1,12 +1,13 @@
 export async function getSignature(str) {  
   const secret = window.localStorage.getItem("token") || "";
-
-  const key = await window.crypto.subtle.importKey("raw", new TextEncoder().encode(secret), {name: "HMAC", hash: "SHA-256"}, false, ["sign"]);
-  const sig = await window.crypto.subtle.sign("HMAC", key, new TextEncoder().encode(str));
-
-  const signature = Array.from(new Uint8Array(sig))
-    .map (b => b.toString (16).padStart (2, "0"))
-    .join ("");
-
-  return signature;
+  if (secret) {
+    const key = await window.crypto.subtle.importKey("raw", new TextEncoder().encode(secret), {name: "HMAC", hash: "SHA-256"}, false, ["sign"]);
+    const sig = await window.crypto.subtle.sign("HMAC", key, new TextEncoder().encode(str));
+  
+    const signature = Array.from(new Uint8Array(sig))
+      .map (b => b.toString (16).padStart (2, "0"))
+      .join ("");
+  
+    return signature;
+  }
 }
